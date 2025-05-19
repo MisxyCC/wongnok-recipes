bedaa191ea3b9615eea72b3d7b333409b850085f
Misxy <supakorn.dev@outlook.com>
2025-05-20 01:23:05 +0700

    Updated the review card.

--- frontend/src/components/ReviewCard.vue
+++ frontend/src/components/ReviewCard.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="flex flex-wrap flex-col md:flex-row"></div>
   <Card v-if="visible" v-model:visible="visible">
     <template #title>Simple Card</template>
@@ -14,4 +14,34 @@
 
 <script setup lang="ts">
 const visible = defineModel<boolean>('visible');
+</script> -->
+
+<template>
+    <Dialog
+      v-if="visible"
+      v-model:visible="visible"
+      modal
+      header="ดูคะแนนรีวิว"
+      class="w-md overflow-auto"
+    >
+     <div class="flex gap-4 p-2" v-for="index in 5" :key="index">
+      <Card v-if="visible" v-model:visible="visible">
+        <template #title>Simple Card</template>
+        <template #content>
+          <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur
+            error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis,
+            culpa ratione quam perferendis esse, cupiditate neque quas!
+          </p>
+        </template>
+      </Card>
+     </div>
+    </Dialog>
+</template>
+
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+
+const visible = defineModel<boolean>('visible');
+const allCards: Ref<number> = ref(5);
 </script>
